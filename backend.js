88ca0bedb29e2a5ac0f7ede7af8c66440525601e
rjbeckwith55 <rjbeckwith55>
2024-02-09 23:23:58 -0600

    updates

--- backend.js
+++ backend.js
@@ -4,17 +4,31 @@ const PORT = process.env.PORT || 3000;
 
 // Hardcoded data
 const houseData = {
-    lat: "lat",
-    long: "long",
-    baths: "1",
-    beds: "3",
-    squareFeet: 1500,
-    year: "1955"
+    imageUrls: ["https://photos.zillowstatic.com/fp/fb45396d6d25773f3930d388b5142bce-cc_ft_960.webp","https://photos.zillowstatic.com/fp/efdd03675579bc46b4442a36cf170e7d-cc_ft_960.webp","https://photos.zillowstatic.com/fp/efdd03675579bc46b4442a36cf170e7d-cc_ft_960.webp"],
+    lat: "40.777617",
+    long: "-89.585493",
+    baths: "8",
+    beds: "5",
+    squareFeet: 8485,
+    year: "1960",
+    lotSize: 10.47
 };
+
+// Enable CORS middleware
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific HTTP methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+    next();
+  });
 
 // Endpoint to fetch house data
 app.get('/fetchHouse', (req, res) => {
     res.json(houseData);
+});
+
+app.get('/', (req, res) => {
+    res.json("{}")
 });
 
 // Start server
